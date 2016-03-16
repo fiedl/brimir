9daeb2233bf4dd6a72a1dc1358d3655f49936a00
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-03-16 15:00:20 +0100

    adding some helper methods from https://github.com/fiedl/brimir/commit/bab2c5d1ab178712da7a012904f1f54093c8c272

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,6 +73,25 @@ class User < ActiveRecord::Base
     User.agents
         .where(notify: true)
   end
+  
+  # Does the email address of this user belong to the ticket system
+  # itself? For example, there might be a user corresponding to
+  # support@example.com.
+  #
+  # This check is needed to prevent email loops. We do not want to
+  # deliver to those email addresses, since they would be received
+  # by the ticket system, again, creating an email loop.
+  #
+  def ticket_system_address?
+    User.ticket_system_addresses.pluck(:id).include? self.id
+  end
+  
+  # Return all users that correspond to email addresses belonging
+  # to the ticket system, e.g. support@example.com.
+  #
+  def self.ticket_system_addresses
+    User.where(email: EmailAddress.pluck(:email))
+  end
 
   def default_localization
     self.time_zone = Tenant.current_tenant.default_time_zone if time_zone.blank?
